73043d78ad6ef07c92b47404780d5cbc2e396e0e
cguibourg-58 <cguibourg-pro@outlook.fr>
2022-01-29 18:57:35 +0100

    update add-assignment

--- src/app/assignments/add-assignment/add-assignment.component.ts
+++ src/app/assignments/add-assignment/add-assignment.component.ts
@@ -16,8 +16,8 @@ export class AddAssignmentComponent implements OnInit {
   nomAssignment?:string;
   matiereAssignment?:string; 
   authorAssignment?:string;
-  descriptionAssignment?:string;
-  dateDeRendu?:Date;
+  remarquesAssignment?:string;
+  dateDeRendu!:Date;
   noteAssignment?:number;
   coefAssignment?:number; 
 
@@ -33,9 +33,57 @@ export class AddAssignmentComponent implements OnInit {
 
     const newAssignment = new Assignment();
     newAssignment.id = Math.round(Math.random()*100000);
-    //newAssignment.nomAssignment = this.nomAssignment;
-    //newAssignment.dateDeRendu = this.dateDeRendu;
+    newAssignment.nom = "(Non renseigné)";
+    if(this.nomAssignment) {
+      newAssignment.nom = this.nomAssignment!;
+    }
+    newAssignment.auteur = "Anonymous";
+    if(this.authorAssignment) {
+      newAssignment.auteur = this.authorAssignment!;
+    }
+    newAssignment.matiere = "Dev Web";
+    if(this.matiereAssignment) {
+      newAssignment.matiere = this.matiereAssignment!;
+    }
+    const todayDate = new Date();
+    var jourDeRenduParDefaut = 1;
+    var moisDuRenduParDefaut = 1;
+    var fullYear = todayDate.getFullYear();
+    const prepareDateParDefaut = () => {
+      var nombreAAjouterPourQueMoisOK = 3;
+      if(!(todayDate.getDate()>28)) {
+        nombreAAjouterPourQueMoisOK--;
+        jourDeRenduParDefaut=todayDate.getDay();
+      }
+      if(!(todayDate.getMonth()+nombreAAjouterPourQueMoisOK>12)) {
+        nombreAAjouterPourQueMoisOK--;
+        fullYear--;
+        moisDuRenduParDefaut=todayDate.getMonth()+nombreAAjouterPourQueMoisOK;
+      }
+    }
+    prepareDateParDefaut();
+    newAssignment.dateDeRendu = new Date(moisDuRenduParDefaut+"/"+jourDeRenduParDefaut+"/"+fullYear);
+    console.log("date de rendu : "+newAssignment.dateDeRendu);
+    if(this.dateDeRendu) {
+      try {
+        newAssignment.dateDeRendu = new Date(this.dateDeRendu);
+      } catch {
+      }
+    }
+    newAssignment.matiere = "Dev Web";
+    if(this.matiereAssignment) {
+      newAssignment.matiere = this.matiereAssignment!;
+    }
+    if(this.remarquesAssignment) {
+      newAssignment.remarques = this.remarquesAssignment!;
+    }
     newAssignment.rendu = false;
+    newAssignment.coefficient = 1;
+    if(this.coefAssignment) {
+      newAssignment.coefficient = this.coefAssignment!;
+    }
+    //seul le professeur peut attribuer une note et ce lors de l'édition de l'assignment
+    newAssignment.note = -1;
 
     this.assignmentService.addAssignment(newAssignment)
     .subscribe(reponse => {
@@ -58,8 +106,31 @@ export class AddAssignmentComponent implements OnInit {
     return true;
   }
 
-  getTeacher(): void {
+  setMatiere(choix:Number): void {
     
+    switch(choix) {
+      case 0: {
+        this.matiereAssignment="Dev Web";
+        break;
+      }
+      case 1: {
+        this.matiereAssignment="Gestion de projet";
+        break;
+      }
+      case 2: {
+        this.matiereAssignment="Création d'entreprise";
+        break;
+      }
+      case 3: {
+        this.matiereAssignment="Management du numérique";
+        break;
+      }
+      default: {
+        this.matiereAssignment="BD pour le Big Data";
+        break;
+      }
+    }
+
     console.log('Matiere ' + this.matiereAssignment + ' séléctionée')
 
   }
